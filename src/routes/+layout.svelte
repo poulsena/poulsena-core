7bfb2c7e3bdcf5f60e8284d4a182b2fe0a75e0e4
Andreas Poulsen <andreas_poulsen@hotmail.com>
2023-09-13 15:53:43 +0200

    Add projects routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 <nav class="flex justify-between items-center p-2 dark:bg-zinc-900 shadow-md mb-2">
 	<div class="flex justify-start space-x-4">
 		<a href="/">Home</a>
+		<a href="/projects">Projects</a>
 		<a href="/about">About</a>
 		<a href="/contact">Contact</a>
 	</div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,1 @@
+<title>poulsena - Projects</title>
